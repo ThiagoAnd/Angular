b1ba98b0c2a2f3cf5fb898647cd2a366147973f2
ThiagoAnd <thiago.1301801@fapi-pinhais.edu.br>
2021-02-04 23:19:41 -0300

    FEAT: Atualização de projeto

--- AngularLoiane/data-binding/src/app/app.module.ts
+++ AngularLoiane/data-binding/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//Para usar o ngModel no input adicionar o forms module
+import { FormsModule }   from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +14,8 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
